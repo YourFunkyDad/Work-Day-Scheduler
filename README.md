229e29aac1f71d70d28917887072a7b235560a4d
Funky Dad <jacobmontgomeryallred@gmail.com>
2022-03-13 14:04:56 -0400

    Updated README

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 # Work-Day-Scheduler
+The application is roughly ~75% complete, but there are a few things I need to fix;
+My local storage is caching the text information I input, but it is not saving the exact value. (See Screenshot Below)
+The text is not persisting when the page is refreshed. (Currently trying to fix)
 
-Having recently completed the Code Quiz for Module 4, completing this Module 5 challenge did not prove to be as headache-inducing as some other challenges. 
-
-I am still not very comfortable with JQuery. I do understand that it is essentially a cleaner, simpler way to condense your JavaScript.
+Having recently completed the Code Quiz for Module 4, completing this Module 5 challenge did not prove to be as headache-inducing as some other challenges. I am still not very comfortable with JQuery. I do understand that it is essentially a cleaner, simpler way to condense your JavaScript.
 
 Having used many references from the internet and essentially triple checking every function along the way, I still have a far ways to go when it comes to getting comfortable writing JQuery. 
 
-My local storage is caching the text information I input, but it is not saving the exact value. (See Screenshot Below)
-
-The application is roughly more than ~80% complete, but there are a few things I would like to add, like the ability to drag and drop the text boxes into the other timeblocks. 
+This application uses Moment.js to handle the time. 
 
 Deployed URL: https://yourfunkydad.github.io/Work-Day-Scheduler/
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -35,34 +35,9 @@ $(document).ready(function () {
     $(".saveBtn").on("click", function () {
         var text = $(this).siblings(".description").val(); 
         var time = $(this).parent().attr("id");
-
-
+        
         localStorage.setItem(time, text);
     })
-
-    function timeTracker() {
-        var timeNow = moment().hour();
-
-        $(".time-block").each(function() {
-            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-
-            if (blockTime < timeNow) {
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-                $(this).addClass("past");
-            }
-            else if (blockTime === timeNow) {
-                $(this).removeClass("future");
-                $(this).removeClass("past");
-                $(this).addClass("present");    
-            }
-            else {
-                $(this).removeClass("past");
-                $(this).removeClass("present");
-                $(this).addClass("future");
-            }
-        })
-    }
 
     $("#hour0 .description").val(localStorage.getItem("hour0"));
     $("#hour1 .description").val(localStorage.getItem("hour1"));
@@ -89,6 +64,28 @@ $(document).ready(function () {
     $("#hour22 .description").val(localStorage.getItem("hour22"));
     $("#hour23 .description").val(localStorage.getItem("hour23"));
 
-    timeTracker();
-    
+    function timeTracker() {
+        var timeNow = moment().hour();
+
+        $(".time-block").each(function() {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("past");
+                $(this).addClass("present");    
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        })
+    }
+    timeTracker();  
 })
